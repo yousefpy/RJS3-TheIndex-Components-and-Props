1927d39df2f494906fbabcb64ae27901e743fa54
Octowl <alsaff1987@gmail.com>
2018-07-29 15:35:44 +0300

    extract components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
+
+// Data
 import authors from "./data";
+
+// Components
+import Sidebar from "./Sidebar";
+import AuthorsList from "./AuthorsList";
 
 class App extends Component {
   render() {
@@ -7,105 +13,10 @@ class App extends Component {
       <div id="app" className="container-fluid">
         <div className="row">
           <div className="col-2">
-            <div id="sidebar">
-              <img src="theindex.svg" className="logo" alt="the index logo" />
-              <section>
-                <h4 className="menu-item active">
-                  <button>AUTHORS</button>
-                </h4>
-              </section>
-            </div>
+            <Sidebar />
           </div>
           <div className="content col-10">
-            <div className="authors">
-              <h3>Authors</h3>
-              <div className="row">
-                <div className="col-lg-4 col-md-6 col-12">
-                  <div className="card">
-                    <div className="image">
-                      <img
-                        className="card-img-top img-fluid"
-                        src={authors[0].imageUrl}
-                        alt={authors[0].first_name + " " + authors[0].last_name}
-                      />
-                    </div>
-                    <div className="card-body">
-                      <h5 className="card-title">
-                        <span>
-                          {authors[0].first_name + " " + authors[0].last_name}
-                        </span>
-                      </h5>
-                      <small className="card-text">
-                        {authors[0].books.length} books
-                      </small>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-4 col-md-6 col-12">
-                  <div className="card">
-                    <div className="image">
-                      <img
-                        className="card-img-top img-fluid"
-                        src={authors[1].imageUrl}
-                        alt={authors[1].first_name + " " + authors[1].last_name}
-                      />
-                    </div>
-                    <div className="card-body">
-                      <h5 className="card-title">
-                        <span>
-                          {authors[1].first_name + " " + authors[1].last_name}
-                        </span>
-                      </h5>
-                      <small className="card-text">
-                        {authors[1].books.length} books
-                      </small>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-4 col-md-6 col-12">
-                  <div className="card">
-                    <div className="image">
-                      <img
-                        className="card-img-top img-fluid"
-                        src={authors[2].imageUrl}
-                        alt={authors[2].first_name + " " + authors[2].last_name}
-                      />
-                    </div>
-                    <div className="card-body">
-                      <h5 className="card-title">
-                        <span>
-                          {authors[2].first_name + " " + authors[2].last_name}
-                        </span>
-                      </h5>
-                      <small className="card-text">
-                        {authors[2].books.length} books
-                      </small>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-4 col-md-6 col-12">
-                  <div className="card">
-                    <div className="image">
-                      <img
-                        className="card-img-top img-fluid"
-                        src={authors[3].imageUrl}
-                        alt={authors[3].first_name + " " + authors[3].last_name}
-                      />
-                    </div>
-                    <div className="card-body">
-                      <h5 className="card-title">
-                        <span>
-                          {authors[3].first_name + " " + authors[3].last_name}
-                        </span>
-                      </h5>
-                      <small className="card-text">
-                        {authors[3].books.length} books
-                      </small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <AuthorsList authors={authors} />
           </div>
         </div>
       </div>
---
+++ src/AuthorsList.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+// Components
+import AuthorCard from "./AuthorCard";
+
+class AuthorsList extends Component {
+  render() {
+    const authorCards = this.props.authors.map(author => (
+      <AuthorCard key={author.first_name + author.last_name} {...author} />
+    ));
+
+    return (
+      <div className="authors">
+        <h3>Authors</h3>
+        <div className="row">{authorCards}</div>
+      </div>
+    );
+  }
+}
+
+export default AuthorsList;
